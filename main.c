41f4ecfb44d009acaefa35bd9c07fa55e8f7c0e2
LUPOSaymon <simone.mija@gmail.com>
2021-12-24 21:50:07 +0000

    -Added Comments -Added malloc

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_primitives.h>
-#include <stdio.h>
 #include <math.h>
+#include <string.h>
+#include <stdio.h>  //Used only if we want to print something
 
 #define SCREENWIDTH 2000
 #define SCREENHEIGHT 1800
-#define NUM_KLUSTERS 15
-#define NUM_POINTS 10000
+#define NUM_CLUSTERS 150         //Number of clusters(Centroids)
+#define NUM_POINTS 250000
+#define RADIUS 1                //Radius of a single point
+#define SQUARE_DIMENSIONS 10    //Dimmension of a single centroid
+#define FRAMES_PER_SECONDS 14   //Numbers of updates done in a single seconds
 
-#define RADIUS 5
-#define SQUARE_DIMENSIONS 10
-#define FRAMES_PER_SECONDS 14
-
-//###Point Struct###
+//###POINT STRUCT###
 typedef struct
 {
     int x,
         y;
-    int klusterId;
+    int clusterId;
     ALLEGRO_COLOR color;
 
 }Point;
-//###Cluster Struct###
+
+//###CLUSTER STRUCT###
 typedef struct
 {
     int id;
@@ -32,25 +33,26 @@ typedef struct
     int allX;
     int allY;
     int numElements;
-}Kluster;
+}Cluster;
 
 
-int initKlustersAndPoints(Point* points, Kluster* klusters);    //Initialize the Kluster Array and the Points Array
-int displayKlusters(Kluster* klusters);                         //Display the Klusters (Centroids) (with filled squares)
-int displayPoints(Point* points);                               //Display the Points on screen (with filled circles)
-ALLEGRO_COLOR updatePoint(Point* point, Kluster* klusters);    //assign to a point the kluster(color & klusterId)
-float distanceBetweenPoints(int x0,int y0, int x1,int y1);
-
-void updateKlusters(Kluster *klusters);
-
-void updatePoints(Point *points, Kluster *klusters);
+int initClustersAndPoints(Point* points, Cluster* clusters);    //Initializes the Cluster Array and the Points Array
+int drawClusters(Cluster* clusters);                            //Displays the clusters (Centroids) (with filled squares)
+int drawPoints(Point* points);                                  //Displays the Points on screen (with filled circles)
+ALLEGRO_COLOR updatePoint(Point* point, Cluster* clusters);     //Assigns to a point the cluster(color & clusterId)
+float distanceBetweenPoints(int x0,int y0, int x1,int y1);      //Calculates distance between two points
+void updateClusters(Cluster *clusters);                         //Updates all clusters positions
+void updatePoints(Point *points, Cluster *clusters);            //Updates all points colors
 
 int main()
 {
     srand(time(NULL));
-    Kluster klusters[NUM_KLUSTERS];
-    Point points[NUM_POINTS];
-    initKlustersAndPoints(points, klusters);
+    Cluster *clusters = malloc(sizeof(Cluster) * NUM_CLUSTERS);
+    Point *points = malloc(sizeof(Point) * NUM_POINTS);
+
+    int iterationCounter = 1;               //Used to print the iterations
+    initClustersAndPoints(points, clusters);
+
     if (!al_init())
     {
         al_show_native_message_box(NULL,NULL,NULL,"Could not inizialize Allegro5",NULL,NULL);
@@ -96,17 +98,21 @@ int main()
 
         if(redraw)
         {
-            al_clear_to_color(al_map_rgb(0, 0, 0));             //Reset Display
-
-            displayPoints(points);
-            displayKlusters(klusters);
-            al_flip_display();
-            updateKlusters(klusters);
-            updatePoints(points,klusters);
+            //Reset Display
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            //Displaying Points and clusters
+            drawPoints(points);
+            drawClusters(clusters);
+            al_flip_display();      //Display all the drew objectes
+            //Updates position of clusters and the points color
+            updateClusters(clusters);
+            updatePoints(points, clusters);
+            //Print the iteration number
+            fprintf(stderr,"Doing the %d iteration\n",iterationCounter);
+            iterationCounter++;
+            //Set the redraw to false
             redraw = false;
         }
-
-
     }
 
     al_destroy_display(display);
@@ -116,75 +122,75 @@ int main()
     return 0;
 }
 
-void updatePoints(Point *points, Kluster *klusters)
+void updatePoints(Point *points, Cluster *clusters)
 {
     for (int i = 0; i < NUM_POINTS; ++i)
     {
-        points[i].color = updatePoint(&points[i],klusters);
+        points[i].color = updatePoint(&points[i],clusters);
     }
 }
 
-void updateKlusters(Kluster *klusters)
+void updateClusters(Cluster *clusters)
 {
 
-    for (int i = 0; i < NUM_KLUSTERS; ++i)
+    for (int i = 0; i < NUM_CLUSTERS; ++i)
     {
-        klusters[i].x = klusters[i].allX / klusters[i].numElements;
-        klusters[i].y = klusters[i].allY / klusters[i].numElements;
-        klusters[i].numElements = 0;
-        klusters[i].allX = 0;
-        klusters[i].allY = 0;
+        clusters[i].x = clusters[i].allX / clusters[i].numElements;
+        clusters[i].y = clusters[i].allY / clusters[i].numElements;
+        clusters[i].numElements = 0;
+        clusters[i].allX = 0;
+        clusters[i].allY = 0;
     }
 
 }
 
-int initKlustersAndPoints(Point* points, Kluster* klusters)
+int initClustersAndPoints(Point* points, Cluster* clusters)
 {
-    //Create Klusters Points
-    for (int i = 0; i < NUM_KLUSTERS; ++i)
+    //Create clusters Points
+    for (int i = 0; i < NUM_CLUSTERS; ++i)
     {
-        klusters[i].id = i;
-        klusters[i].x = rand() % SCREENWIDTH;
-        klusters[i].y = rand() % SCREENHEIGHT;
-        klusters[i].color = al_map_rgb_f(rand() % 256 / (float)256, rand() % 256 / (float)256, rand() % 256 / (float)256);
-        klusters[i].numElements = 0;
-        klusters[i].allX = 0;
-        klusters[i].allY = 0;
+        clusters[i].id = i;
+        clusters[i].x = rand() % SCREENWIDTH;
+        clusters[i].y = rand() % SCREENHEIGHT;
+        clusters[i].color = al_map_rgb_f(rand() % 256 / (float)256, rand() % 256 / (float)256, rand() % 256 / (float)256);
+        clusters[i].numElements = 0;
+        clusters[i].allX = 0;
+        clusters[i].allY = 0;
     }
 
     for (int i = 0; i < NUM_POINTS; ++i)
     {
         points[i].x = rand() % SCREENWIDTH;
         points[i].y = rand() % SCREENHEIGHT;
-        points[i].color = updatePoint(&points[i],klusters);
+        points[i].color = updatePoint(&points[i], clusters);
     }
     return 0;
 }
-ALLEGRO_COLOR updatePoint(Point* point, Kluster* klusters)
+ALLEGRO_COLOR updatePoint(Point* point, Cluster* clusters)
 {
     int idMin = 0;
-    float minDistance = distanceBetweenPoints(point->x,point->y,klusters[0].x, klusters[0].y);
+    float minDistance = distanceBetweenPoints(point->x, point->y, clusters[0].x, clusters[0].y);
     float tempDistance = 0;
-    for (int i = 1; i < NUM_KLUSTERS; ++i)
+    for (int i = 1; i < NUM_CLUSTERS; ++i)
     {
-        tempDistance = distanceBetweenPoints(point->x,point->y,klusters[i].x, klusters[i].y);
+        tempDistance = distanceBetweenPoints(point->x, point->y, clusters[i].x, clusters[i].y);
         if (tempDistance < minDistance)
         {
             minDistance = tempDistance;
             idMin = i;
         }
     }
-    klusters[idMin].allX += point->x;
-    klusters[idMin].allY += point->y;
-    klusters[idMin].numElements++;
-    return klusters[idMin].color;
+    clusters[idMin].allX += point->x;
+    clusters[idMin].allY += point->y;
+    clusters[idMin].numElements++;
+    return clusters[idMin].color;
 }
 float distanceBetweenPoints(int x0,int y0, int x1,int y1)
 {
     return (float)sqrt(pow(x0-x1,2) + pow(y0-y1,2));
 
 }
-int displayPoints(Point* points)
+int drawPoints(Point* points)
 {
     for (int i = 0; i < NUM_POINTS; ++i)
     {
@@ -193,11 +199,11 @@ int displayPoints(Point* points)
     return 0;
 }
 
-int displayKlusters(Kluster* klusters)
+int drawClusters(Cluster* clusters)
 {
-    for (int i = 0; i < NUM_KLUSTERS; ++i)
+    for (int i = 0; i < NUM_CLUSTERS; ++i)
     {
-        al_draw_filled_rectangle((float)klusters[i].x - SQUARE_DIMENSIONS,(float)klusters[i].y - SQUARE_DIMENSIONS ,(float)klusters[i].x + SQUARE_DIMENSIONS,(float)klusters[i].y + SQUARE_DIMENSIONS,klusters[i].color);
+        al_draw_filled_rectangle((float)clusters[i].x - SQUARE_DIMENSIONS, (float)clusters[i].y - SQUARE_DIMENSIONS , (float)clusters[i].x + SQUARE_DIMENSIONS, (float)clusters[i].y + SQUARE_DIMENSIONS, clusters[i].color);
     }
     return 0;
 }
